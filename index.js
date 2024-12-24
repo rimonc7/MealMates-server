bdbea07029c9b41d7caa2787ba3fca15e0578c86
rimonc7 <rimonc7@gmail.com>
2024-12-24 11:07:16 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -2,9 +2,33 @@ const express = require('express');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 
+app.use(cors());
+app.use(express.json());
 
+const uri = 'mongodb+srv://job_hunter:WFylTUHg2zhWQV50@cluster0.by2cb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 
 
